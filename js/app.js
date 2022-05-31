79a17d9b7daf607e0fa3ea66a92313d1081964be
Ina Bostrøm <ina_b86@hotmail.com>
2022-05-31 19:01:33 +0200

    forside

--- js/app.js
+++ js/app.js
@@ -34,13 +34,84 @@ async function getPost(pageValue) {
   console.log(post);
 
   const { result } = await post.json();
+  console.log("JSON from project:")
+  console.log(result);
   // her legger vi inn project variabel ferdig <img> element returnert fra støtte funksjon som håndtere bilder
   project.append(handleImage(result[0].mainImage.asset._ref, 400));
   const title = document.createElement("h1");
   title.innerText = result[0].title;
   project.append(title);
   project.append(handleParagraphs(result[0].body));
+  await getColorSections(result[0].colors);
 }
+
+
+async function getColorSections(colorsSchemaList) {
+  let colorSection = document.querySelector(".colors");
+  console.log(colorSection);
+
+
+  colorsSchemaList.forEach(async element => {
+    let colorSchema2 = await fetch(`https://${projectID}.api.sanity.io/v1/data/query/production?query=*
+    [_id == '${element._ref}']`);
+    const { result } = await colorSchema2.json();
+  
+    console.log("info:");
+    console.log(result);
+    //console.log(element._ref);
+
+    let divElement = document.createElement("div");
+    divElement.classList.add("color-element");
+
+    if(result[0].title != null){
+      let headerElement = document.createElement("p");
+      headerElement.classList.add("color-sub-header");
+      headerElement.textContent = result[0].title;
+      divElement.append(headerElement);
+    }
+
+    let bodyElement = GetPValuesOfBody(result[0].body)
+
+    divElement.append(bodyElement);
+    let imageElement = document.createElement("div");
+    imageElement.classList.add("color-image");
+    if(result[0].image != null){
+      imageElement.append(handleImage(result[0].image.asset._ref, 400, "color-image-inner"))
+      divElement.append(imageElement);
+    }
+    colorSection.appendChild(divElement);
+
+  });
+}
+
+function GetPValuesOfBody(body) {
+  let contentBox = document.createElement("div");
+
+  if(body != null){
+    body.forEach((bodyElement) => {
+      let contentPelement = document.createElement("p");
+      contentPelement.classList.add("styleForP");
+     
+      bodyElement.children.forEach((child) => {
+        
+        if(child.marks.includes("strong")){
+          let strongElement = document.createElement("b")
+          strongElement.textContent = child.text;
+          contentPElement.appendChild(strongElement);
+        }else {
+          let spanElement = document.createElement("span");
+          spanElement.textContent += child.text;
+          contentPElement.appendChild(spanElement);
+        }
+      });
+
+      contentBox.appendChild(contentPElement);
+    });
+    
+  }
+  return contentBox;
+}
+
 
 async function getAuthors() {
   const authors =
@@ -117,6 +188,20 @@ async function getAuthors() {
 
     aboutMeElement.append(shortIntroElement); //Legger div elementet inn i div med about-me id, i selve index.html
   }
+
+  //insight fra sanity
+
+  let insightElement = document.getElementById("insight");
+  if (insightElement !== null) {
+
+  //  insightElement.append(
+  //     handleImage(result[0].image.asset._ref, 400, "om-meg-img")
+  //   ); //Setter inn størrelse for bildet
+
+  }
+
+
+
 
   
   // legger til tekst på aboutme.html
